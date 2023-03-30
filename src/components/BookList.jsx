1bd9ee0852d84eca549eb2aaaf56fd681a3417a4
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-30 17:40:38 +0200

    d3

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleBook from "./SingleBook";
 import fantasy from "../data/coverbooks/fantasy.json";
 
 const BookList = () => {
+  const [cercaLibro, setcercaLibro] = useState("");
+
+  const filterBookList = (book) => {
+    return book.title.toLowerCase().includes(cercaLibro.toLowerCase());
+  };
+
   return (
     <div>
-      {fantasy.map((book) => (
+      {}
+      <input
+        type="text"
+        placeholder="Cerca un libro"
+        value={cercaLibro}
+        onChange={(e) => setcercaLibro(e.target.value)}
+      />
+      {}
+      {fantasy.filter(filterBookList).map((book) => (
         <SingleBook key={book.asin} book={book} />
       ))}
     </div>
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -4,7 +4,10 @@ import { Card } from "react-bootstrap";
 const SingleBook = (props) => {
   return (
     <Card>
-      <Card.Img className=" mx-auto d-block img-fluid d-block w-50 " src={props.book.image} />
+      <Card.Img
+        className=" mx-auto d-block img-fluid d-block w-50 "
+        src={props.book.img}
+      />
       <Card.Body>
         <Card.Title>{props.book.title}</Card.Title>
       </Card.Body>
